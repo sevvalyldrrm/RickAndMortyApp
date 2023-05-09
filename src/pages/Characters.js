f8e8b4284af95162454bfdea8bdae21221257006
sevvalyldrrm <ysevvalyldrm@gmail.com>
2023-05-09 18:01:37 +0300

    third

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,21 +1,28 @@
-import { View, Text, Button, FlatList } from 'react-native';
-import React , {useState} from 'react';
+import {View, Text, Button, FlatList, StyleSheet, Image} from 'react-native';
+import React, {useState} from 'react';
 import axios from 'axios';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
 export const CharacterCard = ({character}) => {
- return (
-  <View>
-    <Text>{character.name}</Text>
-  </View>
- )
-}
+  return (
+    <TouchableWithoutFeedback>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{uri: character.image}} />
+        <View style={styles.bodyContainer}>
+          <Text>{character.name}</Text>
+          <Text>{character.status}</Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const Characters = () => {
-  const [character, setCharacter] = useState([])
+  const [character, setCharacter] = useState([]);
 
   async function fetchData() {
     const response = await axios.get(
-      'https://rickandmortyapi.com/api/character'
+      'https://rickandmortyapi.com/api/character',
     );
     setCharacter(response.data.results);
 
@@ -24,20 +31,25 @@ const Characters = () => {
     // );
   }
 
-  const renderItem = ({item}) => (
-    <CharacterCard character={item} />
-  )
+  const renderItem = ({item}) => <CharacterCard character={item} />;
 
   return (
     <View>
-      <FlatList 
-      data={character}
-      renderItem= {renderItem}
-      keyExtractor={(item) => item.id.toString()}
+      <FlatList
+        data={character}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
       />
-      <Button title='Fetch Data' onPress={fetchData}/>
+      <Button title="Fetch Data" onPress={fetchData} />
     </View>
-  )
-}
+  );
+};
 
 export default Characters;
+
+const styles = StyleSheet.create({
+  image : {
+    width : 50,
+    height : 50
+  }
+});
